431656d06fb1fc3e3d88e26010260b86507b5430
Zach <zach.morrato@outlook.com>
2025-02-17 20:23:56 -0700

    yes

--- Learning Files/Graphics/Pointer/main.cpp
+++ Learning Files/Graphics/Pointer/main.cpp
@@ -1,7 +1,18 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
+#include <cstdlib>
 #include <cmath>
-#include <array>
+#include <vector>
+#include <chrono>
+
+
+struct Bullet{
+    sf::Vector2f direction;
+    float speed = 2000;
+
+    sf::CircleShape bullet = sf::CircleShape(5.0f);
+};
 
 int main()
 {
@@ -18,7 +29,14 @@ int main()
 
     sf::VertexArray laser(sf::PrimitiveType::Lines, 2);
     laser[0].position = sf::Vector2f(500,500);
-    
+
+    std::vector<Bullet> bullets;
+
+    sf::Clock clock;
+
+    sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile("gunshot.wav"))
+        return -1;
 
     while (window.isOpen())
     {
@@ -37,11 +55,49 @@ int main()
 
                 laser[1].position = sf::Vector2f(MousePos.x, MousePos.y);
             }
+            if(event->is<sf::Event::MouseButtonPressed>()){
+                Bullet nBullet;
+                nBullet.direction = sf::Vector2f(laser[1].position.x - laser[0].position.x, laser[1].position.y - laser[0].position.y);
+                nBullet.bullet.setPosition(laser[0].position);
+
+                float magnitude = std::sqrt(nBullet.direction.x * nBullet.direction.x + nBullet.direction.y * nBullet.direction.y);
+                if (magnitude != 0){
+                    nBullet.direction.x /= magnitude;
+                    nBullet.direction.y /= magnitude;
+                }
+
+                bullets.push_back(nBullet);
+
+                sf::Sound(buffer).play();
+                
+            }
         }
 
+        sf::Time dTime = clock.restart();
+        float dt = dTime.asSeconds();
+
         window.clear();
-        window.draw(triangle);
         window.draw(laser);
+        window.draw(triangle);
+
+
+        for (auto it = bullets.begin(); it != bullets.end();){
+            
+            float newX = it->bullet.getPosition().x + (it->direction.x * it->speed * dt);
+            float newY = it->bullet.getPosition().y + (it->direction.y * it->speed * dt);
+
+            it->bullet.setPosition(sf::Vector2f(newX, newY));
+
+            if (newX < 0 || newX > window.getSize().x || newY < 0 || newY > window.getSize().y){
+                it = bullets.erase(it);
+            }else {
+                window.draw(it->bullet);
+                ++it;
+            }
+
+            
+        };
+
         window.display();
     }
 
